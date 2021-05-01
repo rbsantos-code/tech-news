cc525d51acfb829a6c329bfbbd06610ad1bef52a
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-01 13:48:27 -0700

    create more routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -41,4 +41,43 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [ 
+         'id',
+         'post_url', 
+         'title', 
+         'created_at', 
+         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.get({ plain: true });
+
+        res.render('edit-post', { posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
